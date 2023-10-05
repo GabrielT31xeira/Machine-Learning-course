0836522df3d94a51fff54b748c7e3ff7c506c30c
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-10-05 14:21:42 -0300

    Splitting datase in training and test set

--- lession_01/lession01.R
+++ lession_01/lession01.R
@@ -16,5 +16,13 @@ print(dataset)
 
 # Encoding categorical data
 dataset$Country <- factor(dataset$Country, levels = c('France','Spain' , 'Germany'), labels = c(1, 2, 3))
-dataset$Purchased <- factor(dataset$Purchased, levels = c('Yes', 'No'), labels = c(0, 1))
+dataset$Purchased <- factor(dataset$Purchased, levels = c('No', 'Yes'), labels = c(0, 1))
 print(dataset)
+
+# Splitting datase in training and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
