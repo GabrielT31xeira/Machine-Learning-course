36166ad9f2043a9c1fe7346f31340e69b614ea44
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-10-06 14:56:03 -0300

    Update lession01.R

--- lession_01/lession01.R
+++ lession_01/lession01.R
@@ -1,23 +1,22 @@
 # Import dataset
 # Remenber dont use "\" for import datasets
 dataset <- read.csv("C:/Users/gt3ix/PycharmProjects/pythonProject/lession_01/data/Data.csv")
-print(dataset)
+# dataset <- dataset[, 2:3]
 
 # Taking care of missing data
-dataset$Age <- ifelse(is.na(dataset$Age),
-                      ave(dataset$Age, FUN = function (x) mean(x, na.rm = TRUE)),
-                      dataset$Age)
+# dataset$Age <- ifelse(is.na(dataset$Age),
+#                       ave(dataset$Age, FUN = function (x) mean(x, na.rm = TRUE)),
+#                       dataset$Age)
+#
+# dataset$Salary <- ifelse(is.na(dataset$Salary),
+#                       ave(dataset$Salary, FUN = function (x) mean(x, na.rm = TRUE)),
+#                       dataset$Salary)
 
-dataset$Salary <- ifelse(is.na(dataset$Salary),
-                      ave(dataset$Salary, FUN = function (x) mean(x, na.rm = TRUE)),
-                      dataset$Salary)
-
-print(dataset)
 
 # Encoding categorical data
-dataset$Country <- factor(dataset$Country, levels = c('France','Spain' , 'Germany'), labels = c(1, 2, 3))
-dataset$Purchased <- factor(dataset$Purchased, levels = c('No', 'Yes'), labels = c(0, 1))
-print(dataset)
+# dataset$Country <- factor(dataset$Country, levels = c('France','Spain' , 'Germany'), labels = c(1, 2, 3))
+# dataset$Purchased <- factor(dataset$Purchased, levels = c('No', 'Yes'), labels = c(0, 1))
+# print(dataset)
 
 # Splitting datase in training and test set
 #install.packages('caTools')
@@ -29,5 +28,5 @@ test_set <- subset(dataset, split == FALSE)
 
 # Feature Scaling
 
-training_set[, 2:3] <- scale(training_set[, 2:3])
-test_set[, 2:3] <- scale(test_set[, 2:3])
+# training_set[, 2:3] <- scale(training_set[, 2:3])
+# test_set[, 2:3] <- scale(test_set[, 2:3])
